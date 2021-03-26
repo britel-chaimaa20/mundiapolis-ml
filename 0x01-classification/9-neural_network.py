878bd337456c78120fa77740fe6965ced990c622
Britel Chaimaa <52888345+britel-chaimaa20@users.noreply.github.com>
2021-03-26 22:20:49 +0100

    Create 9-neural_network.py

---
+++ 0x01-classification/9-neural_network.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+"""
+Module contenant la classe neurone
+"""
+import numpy as np
+
+
+class NeuralNetwork:
+    """
+    Une classe qui définit un réseau de neurones avec une couche cachée effectuant une classification binaire
+    """
+
+    def __init__(self, nx, nodes):
+        """
+        constructeur de la classe
+        la variable nx: est le nombre d'entités d'entrée du neurone
+        """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.nx = nx
+        if type(nodes) is not int:
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+        # hidden layer
+        self.__W1 = np.random.randn(nodes, nx)
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+        # output neuron
+        self.__W2 = np.random.randn(1, nodes)
+        self.__b2 = 0
+        self.__A2 = 0
+
+    @property
+    def W1(self):
+        return self.__W1
+
+    @property
+    def b1(self):
+        return self.__b1
+
+    @property
+    def A1(self):
+        return self.__A1
+
+    @property
+    def W2(self):
+        return self.__W2
+
+    @property
+    def b2(self):
+        return self.__b2
+
+    @property
+    def A2(self):
+        return self.__A2
