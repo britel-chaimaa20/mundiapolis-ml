e735e1cc46b7f960889a4bc42bd9328bd957dfde
Britel Chaimaa <52888345+britel-chaimaa20@users.noreply.github.com>
2021-04-03 22:19:56 +0100

    Update 9-neural_network.py

--- 0x01-classification/9-neural_network.py
+++ 0x01-classification/9-neural_network.py
@@ -1,58 +1,57 @@
 #!/usr/bin/env python3
-"""
-Module contenant la classe neurone
-"""
+'''Neural Network'''
 import numpy as np
 
 
-class NeuralNetwork:
-    """
-    Une classe qui définit un réseau de neurones avec une couche cachée effectuant une classification binaire
-    """
+class NeuralNetwork():
+    '''Neural Network defines a neural
+    network with one hidden layer
+    performing binary classification'''
 
     def __init__(self, nx, nodes):
-        """
-        constructeur de la classe
-        la variable nx: est le nombre d'entités d'entrée du neurone
-        """
-        if type(nx) is not int:
+        '''Class Constructor'''
+        if type(nx) != int:
             raise TypeError("nx must be an integer")
-        if nx < 1:
+        elif nx < 1:
             raise ValueError("nx must be a positive integer")
-        self.nx = nx
-        if type(nodes) is not int:
+        elif type(nodes) != int:
             raise TypeError("nodes must be an integer")
-        if nodes < 1:
+        elif nodes < 1:
             raise ValueError("nodes must be a positive integer")
-        # hidden layer
-        self.__W1 = np.random.randn(nodes, nx)
-        self.__b1 = np.zeros((nodes, 1))
-        self.__A1 = 0
-        # output neuron
-        self.__W2 = np.random.randn(1, nodes)
-        self.__b2 = 0
-        self.__A2 = 0
+        else:
+            self.__W1 = np.random.normal(size=(nodes, nx))
+            self.__b1 = np.array([[np.array(0.)]] * nodes)
+            self.__A1 = 0
+            self.__W2 = np.random.normal(size=(1, nodes))
+            self.__b2 = 0
+            self.__A2 = 0
 
     @property
     def W1(self):
+        '''W'''
         return self.__W1
 
     @property
     def b1(self):
+        '''W'''
         return self.__b1
 
     @property
     def A1(self):
+        '''W'''
         return self.__A1
 
     @property
     def W2(self):
+        '''W'''
         return self.__W2
 
     @property
     def b2(self):
+        '''W'''
         return self.__b2
 
     @property
     def A2(self):
+        '''W'''
         return self.__A2
