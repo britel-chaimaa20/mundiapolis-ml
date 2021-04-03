da13647b857b385ba61b3af1a1e4760c9da6d65a
Britel Chaimaa <52888345+britel-chaimaa20@users.noreply.github.com>
2021-04-03 22:21:54 +0100

    Update 10-neural_network.py

--- 0x01-classification/10-neural_network.py
+++ 0x01-classification/10-neural_network.py
@@ -1,70 +1,72 @@
 #!/usr/bin/env python3
-"""
-Module contenant la classe neurone
-"""
+'''Neural Network'''
 import numpy as np
 
 
-class NeuralNetwork:
-    """
-    Une classe qui définit un réseau de neurones avec une couche cachée effectuant une classification binaire
-    """
-    
-    def __init__(self, nx, nodes):
-        """
-        constructeur de la classe
-        la variable nx: est le nombre d'entités d'entrée du neurone
-        """
-        if type(nx) is not int:
-            raise TypeError("nx must be an integer")
-        if nx < 1:
-            raise ValueError("nx must be a positive integer")
-        self.nx = nx
-        if type(nodes) is not int:
-            raise TypeError("nodes must be an integer")
-        if nodes < 1:
-            raise ValueError("nodes must be a positive integer")
-        # hidden layer
-        self.__W1 = np.random.randn(nodes, nx)
-        self.__b1 = np.zeros((nodes, 1))
-        self.__A1 = 0
-        # output neuron
-        self.__W2 = np.random.randn(1, nodes)
-        self.__b2 = 0
-        self.__A2 = 0
+class NeuralNetwork():
+    '''Neural Network defines a neural
+    network with one hidden layer
+    performing binary classification'''
 
-    def forward_prop(self, X):
-        """
-        Le calcule de la propagation directe du réseau de neurones
-         Le paramètre X: tableau np avec les données d'entrée de forme (nx, m)
-         elle retourne : les attributs privés __A1 et __A2
-        """
-        z1 = np.matmul(self.__W1, X) + self.__b1
-        self.__A1 = 1 / (1 + np.exp(-z1))
-        z2 = np.matmul(self.__W2, self.__A1) + self.__b2
-        self.__A2 = 1 / (1 + np.exp(-z2))
-        return self.__A1, self.__A2
+    def __init__(self, nx, nodes):
+        '''Class Constructor'''
+        if type(nx) != int:
+            raise TypeError("nx must be an integer")
+        elif nx < 1:
+            raise ValueError("nx must be a positive integer")
+        elif type(nodes) != int:
+            raise TypeError("nodes must be an integer")
+        elif nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+        else:
+            self.__W1 = np.random.normal(size=(nodes, nx))
+            self.__b1 = np.array([[np.array(0.)]] * nodes)
+            self.__A1 = 0
+            self.__W2 = np.random.normal(size=(1, nodes))
+            self.__b2 = 0
+            self.__A2 = 0
 
     @property
     def W1(self):
+        '''W'''
         return self.__W1
 
     @property
     def b1(self):
+        '''W'''
         return self.__b1
 
     @property
     def A1(self):
+        '''W'''
         return self.__A1
 
     @property
     def W2(self):
+        '''W'''
         return self.__W2
 
     @property
     def b2(self):
+        '''W'''
         return self.__b2
 
     @property
     def A2(self):
+        '''W'''
         return self.__A2
+
+    def forward_prop(self, X):
+        '''Calculates the forward
+        propagation of the neural network'''
+        self.__A1 = self.sigmoid(
+            np.matmul(self.__W1, X) + self.__b1
+        )
+        self.__A2 = self.sigmoid(
+            np.matmul(self.__W2, self.__A1) + self.__b2
+        )
+        return (self.__A1, self.__A2)
+
+    def sigmoid(self, X):
+        '''Sigmoid function'''
+        return 1 / (1 + np.exp(-X))
