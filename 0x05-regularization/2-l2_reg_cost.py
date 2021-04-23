4b86c76d5c2e4522e245f287248070753445a66a
Britel Chaimaa <52888345+britel-chaimaa20@users.noreply.github.com>
2021-04-23 22:40:16 +0000

    Create 2-l2_reg_cost.py

---
+++ 0x05-regularization/2-l2_reg_cost.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""L2 Regularization"""
+
+import tensorflow as tf
+
+
+def l2_reg_cost(cost):
+    """Calculates the cost of a neural network with L2 regularization.
+        Args:
+            cost (tensor): contains the cost of the network without L2
+                           regularization.
+        Returns:
+            tensor: the cost of the network accounting for L2 regularization.
+    """
+    return cost + tf.losses.get_regularization_losses()
